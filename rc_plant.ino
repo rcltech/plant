6b1a91bae664aab7165e18e67b9d0eaea0c4ba14
utkarsh867 <utkarsh867@gmail.com>
2019-03-20 22:19:54 +0800

    Add soil moisture

--- rc_plant.ino
+++ rc_plant.ino
@@ -16,23 +16,10 @@ void setup() {
 }
 
 void loop() {
-<<<<<<< HEAD
   humidity = dht.readHumidity();
   soil_moisture = analogRead(A0);
   
   Serial.println(humidity, 2);
   Serial.println(soil_moisture, DEC);
-=======
-  // air humidity sensor
-  float a = dht.readHumidity();
-  Serial.println(a, 2);
-
-  // soil moisture calibration
-  val = anologRead(A0);
-  Serial.println(val);
-
-
-
->>>>>>> e680e7aecf54d98f659302e2598dbc1125552265
   delay(1000);
 }
