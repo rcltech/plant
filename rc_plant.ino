7f5c140d40338d24c51605086749cd21389cf2bb
Sean Chok <seanchok@gmail.com>
2019-03-20 22:32:57 +0800

    write tasks for Krishna

--- rc_plant.ino
+++ rc_plant.ino
@@ -7,7 +7,7 @@ int soil_moisture = 0;
 float humidity = 0.0;
 
 // soil moisture sensor
-int val;
+// set constant values for dry and wet at 500 and 300 respectively
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,10 +16,18 @@ void setup() {
 }
 
 void loop() {
+  // read sensors
   humidity = dht.readHumidity();
   soil_moisture = analogRead(A0);
-  
+
+  // print outputs
   Serial.println(humidity, 2);
   Serial.println(soil_moisture, DEC);
+
+  // write algorithm to process soil moisture data
+
+  // output relative soil moisture as percentage
+
+
   delay(1000);
 }
